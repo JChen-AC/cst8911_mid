f1243740b604459c88a788cbb506baddb0fa4689
JChen-AC <chen1101@algonquinlive.com>
2026-02-17 23:54:05 -0500

    Getting the setup script to work, tested on a ubuntu vm

--- setup.sh
+++ setup.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
+set -ex
 # update packages
 sudo apt update -y
 
-sudo apt-get install python
-pip install virtualenv
+sudo apt install python3-pip -y
+sudo apt install python3.12-venv -y
+sudo apt install git -y
 
+mkdir app
+cd ./app
 
+git clone web_app
 
-git clone https:GITLINK /web_app
+cd ./web_app
+python3 -m venv .venv 
 
-cd /web_app
-python -m venv .venv 
-
-./venv/Scripts/activate
+source ./.venv/bin/activate
 pip install -r requirements.txt
 
-python ./vm_app.py
+nohup python ./vm_app.py > flask.log >2>&1 &
+
+# HOW DOES IT HANDLE ASKING FOR USER PASSWORD
