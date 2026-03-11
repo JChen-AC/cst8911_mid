3fb334d97e26fa5556e9c0d5d8eaeac83b2eb6f5
JChen-AC <chen1101@algonquinlive.com>
2026-03-11 15:03:38 -0400

    Adding blob test to vm_app

--- vm_app.py
+++ vm_app.py
@@ -9,6 +9,24 @@
 from flask import Flask, jsonify, request, abort
 import time
 import socket 
+import os
+from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+import numpy as np
+
+try:
+    print("Getting Authentication")
+    account_url = "https://cst8911test.blob.core.windows.net"
+    default_credential = DefaultAzureCredential()
+    blob_service_client = BlobServiceClient(account_url, credential=default_credential)
+    token = default_credential.get_token("https://storage.azure.com/.default")
+    input_container_client = blob_service_client.get_container_client("inputs")
+    output_container_client = blob_service_client.get_container_client("outputs")
+
+
+except Exception as ex:
+    print('ERROR:')
+    print(ex)
 
 app = Flask(__name__)
 
@@ -32,6 +50,43 @@ def cpu_test():
         total += i
     
     return jsonify({"message":f"CPU Test completed : serveved by {socket.gethostname()}"}),201
+
+# Send process request 
+@app.route('/blob_test',methods=['GET'])
+def blob_test():
+    try:
+        LOCAL_PATH = "./blob_test_download"
+        os.makedirs(LOCAL_PATH,exist_ok=True)
+
+        ## LIST BLOB NAMES ##
+        print("Reading contianer")
+        blob_list = input_container_client.list_blobs()
+        processed_blob = ""
+        for blob in blob_list:
+            processed_blob = "processed_" + blob.name
+            processed_path = os.path.join(LOCAL_PATH,processed_blob)
+            temp = input_container_client.download_blob(blob.name).readall() 
+            str_arr = temp.decode('utf-8')
+            np_arr = np.array(str_arr.split(','),dtype=int)
+            sort_arr = np.sort(np_arr)
+            np.savetxt(processed_path, sort_arr.reshape(1,-1), delimiter=",", fmt="%d")
+
+        ### WRITE TEST ###
+        print("UPLOADING BLOB")
+        new_blob = blob_service_client.get_blob_client(container="outputs",blob=processed_blob)
+        # Upload the created file
+        with open(file=processed_path, mode="rb") as data:
+            new_blob.upload_blob(data)
+        os.remove(processed_path)
+
+        return jsonify({"message":f"CPU Test completed : serveved by {socket.gethostname()}"}),201
+    except Exception as ex:
+        print('ERROR:')
+        print(ex)
+        return jsonify({"ERROR":f"Blob test failed :error caused by {ex}"}),502
+
+    
+
 
 # Entry point for running the Flask app
 # The app will run on host 0.0.0.0 (accessible on all network interfaces) and port 8000.
