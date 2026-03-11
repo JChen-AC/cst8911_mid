e8bfd24d0854d1f33369a3ce0d094bd9eca8ef60
JChen-AC <chen1101@algonquinlive.com>
2026-03-11 14:29:19 -0400

    Fixing test script to get response and wait until it gets a response

--- test.py
+++ test.py
@@ -4,9 +4,11 @@ import threading
 url = "http://LOAD_BALANCER_PUBLIC_IP:8000/test"
 
 def send():
-    while True:
-        requests.get(url)
+    response = None
+    while response is None:
+        response = requests.get(url)
 
 for i in range(20):
+    print(f"Creating thread: {i}")
     threading.Thread(target=send).start()
 
