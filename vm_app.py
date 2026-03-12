2eee729c79d47f2c3e4978ed77812c4c54af6685
JChen-AC <chen1101@algonquinlive.com>
2026-03-12 14:34:00 -0400

    Specifically specifying what MI to use and fixing the account url to use the one we actually need to use

--- vm_app.py
+++ vm_app.py
@@ -10,14 +10,14 @@ from flask import Flask, jsonify, request, abort
 import time
 import socket 
 import os
-from azure.identity import DefaultAzureCredential
+from azure.identity import DefaultAzureCredential, ManagedIdentityCredential
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 import numpy as np
 
 try:
     print("Getting Authentication")
-    account_url = "https://cst8911test.blob.core.windows.net"
-    default_credential = DefaultAzureCredential()
+    account_url = "https://8911midterm.blob.core.windows.net"
+    default_credential = ManagedIdentityCredential(client_id="a5f2987b-e549-4953-a909-063a9b659fc1")
     blob_service_client = BlobServiceClient(account_url, credential=default_credential)
     token = default_credential.get_token("https://storage.azure.com/.default")
     input_container_client = blob_service_client.get_container_client("input")
@@ -73,7 +73,7 @@ def blob_test():
 
         ### WRITE TEST ###
         print("UPLOADING BLOB")
-        new_blob = blob_service_client.get_blob_client(container="outputs",blob=processed_blob)
+        new_blob = blob_service_client.get_blob_client(container="output",blob=processed_blob)
         # Upload the created file
         with open(file=processed_path, mode="rb") as data:
             new_blob.upload_blob(data)
